8e9c6da3181e47ff2c1437b69aa6a4f08aa8ea93
João Pedro Barbosa <joaopedrofbarbosa@protonmail.com>
2022-10-05 11:05:34 -0300

    chore: rename and improve

--- main.c
+++ main.c
@@ -1,59 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-#define MAX 500
+#define MAXIMUM 512
 
-int *matrices[MAX];
+typedef
+  enum
+  {
+    false, true
+  } boolean;
 
-int main(int argc, char* argv[]) {
-	if (argc > 2) {
-		printf("Too many arguments supplied.");
+int *matrices[MAXIMUM];
+
+int main(int argument_count, char* argument_values[]) {
+	if (argument_count > 2) {
+		printf("Error: Too many arguments given");
 	}
 	else {
-		if (argc == 2) {
-			if (strlen(argv[1]) > 4) {
-				if (strcmp(argv[1] + strlen(argv[1]) - 4, ".txt") != 0) {
+		if (argument_count == 2) {
+			if (strlen(argument_values[1]) > 4) {
+				if (strcmp(argument_values[1] + strlen(argument_values[1]) - 4, ".txt") != 0) {
 					printf("Error: File is not a .txt file\n");
 					return 1;
 				}
 			}
 			else {
-				printf("Error: File is not a .txt file\n");
+				printf("Error: Invalid file given\n");
 				return 1;
 			}
 
+			FILE* file = fopen(argument_values[1], "r");
 
-			FILE* input_file = fopen(argv[1], "r");
-
-			if (input_file == NULL) {
-				printf("Error opening file\n");
+			if (file == NULL) {
+				printf("Error: File could not be opened\n");
 				return 1;
 			}
 			else {
-				char *line = NULL;
-				size_t len = 0;
-				ssize_t line_length;
+        const size_t line_size = 256;
+				char *line = malloc(line_size);
+				size_t length = 0;
+				size_t line_length;
 				int* equation_values;
-				int in_insert_equation_mode = 0; // false
+				boolean inserting_equations = false;
 
-				while ((line_length = getline(&line, &len, input_file)) != -1) {
-					printf("Retrieved line of length %zu:\n", line_length);
-    			printf("%s", line);
+				// while ((line_length = getline(&line, &length, file)) != -1) {
+        // TODO: read line properly
+				while (fgets(line, line_size, file) != NULL) {
+					printf("Retrieved line of length %zu:\n", strlen(line));
+                    printf("%s", line);
 
-					if (line_length < 2) {
+					/* if (line_length < 2) {
 						// store_equation(equation_values);
 						for (int i = 0; i < sizeof(equation_values); i++) {
 							printf("%d", equation_values[i]);
 						}
 
-						in_insert_equation_mode = 0;
+						inserting_equations = 0;
 						if (equation_values)
 							free(equation_values);
 					}
 
-					if (line_length >= 6 && in_insert_equation_mode != 0) {
+					else if (line_length >= 6 && inserting_equations != 0) {
 						for (int i = 0; i < 5; i++) {
 							if (isspace(line[i]) == 0) {
 								int value = (int)line[i];
@@ -62,14 +71,14 @@ int main(int argc, char* argv[]) {
 						}
 					}
 
-					if (line_length == 2 && in_insert_equation_mode == 0) {
+					else if (line_length == 2 && inserting_equations == 0) {
 						int qtd_matrix_rows = (int)line[0];
 						equation_values = (int*)malloc(sizeof(int)*(qtd_matrix_rows*3));
-						in_insert_equation_mode = 1; // true
-					}
+						inserting_equations = true;
+					} */
 				}
 
-				fclose(input_file);
+				fclose(file);
 
 				if (line)
 					free(line);
@@ -77,22 +86,40 @@ int main(int argc, char* argv[]) {
 		}
 		else {
 			menu();
-			
 		}
 	}
+
 	return 0;
 }
 
 void menu() {
-	printf("
-	 _______                          
-	|     __|.---.-.--.--.-----.-----.
-	|    |  ||  _  |  |  |__ --|__ --|
-	|_______||___._|_____|_____|_____|                                  
-	");
-	printf("0 - Sair\n");
-	printf("1 - Escrever sistema\n\n");
-	printf("> Escolha uma opção: ");
+	printf(
+	"_______\n"
+	"|     __|.---.-.--.--.-----.-----.\n"
+	"|    |  ||  _  |  |  |__ --|__ --|\n"
+	"|_______||___._|_____|_____|_____|\n\n"
+	);
+
+  while (true) {
+    printf("0 - Exit\n");
+    printf("1 - Type in equations system\n\n");
+
+    char option;
+
+    printf("> Chose an option: ");
+    scanf("%c", &option);
+
+    if (option == '0')
+      break;
+
+    else if (option == '1')
+      printf("> Type in the number of equations: ");
+
+    else
+      printf("\n> Invalid option!\n\n");
+  }
+
+  return 0;
 }
 
 void store_equation(int *equation_values) {
@@ -101,4 +128,4 @@ void store_equation(int *equation_values) {
 
 
 // https://stackoverflow.com/questions/10874374/passing-a-unknown-size-matrix-reference-to-a-c-function
-// https://www.tutorialspoint.com/cprogramming/c_array_of_pointers.htm		
+// https://www.tutorialspoint.com/cprogramming/c_array_of_pointers.htm
