5cd48efef9b4a663ed86b9406b879cadd3e08a67
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-10-12 18:26:37 -0300

    chore: organize sequence of methods

--- main.c
+++ main.c
@@ -38,6 +38,105 @@ void pause()
   getchar();
 }
 
+float * sanitize_input_equation(char *equation, int qtd_equations) { }
+
+boolean equal_results_between_pairs_of_lines(float *matrix, int matrix_order)
+{
+  float *lines_division = (float *)malloc(sizeof(float) * (matrix_order * matrix_order));
+  int lines_division_position = 0;
+
+  // i - first line of couple
+  // j - second line of couple
+  // k - interator to divide each element
+  for (int i = 0; i < matrix_order; i++)
+  {
+    for (int j = i + 1; j < matrix_order; j++)
+    {
+      for (int k = 0; k < matrix_order; k++)
+      {
+        float result = matrix[i * (matrix_order + 1) + k] / matrix[j * (matrix_order + 1) + k];
+        lines_division[lines_division_position] = result;
+        lines_division_position++;
+      }
+    }
+  }
+
+  int qtd_appears_same_number = 0;
+  for (int i = 0; i < (matrix_order * matrix_order) - 1; i++)
+  {
+    if (lines_division[i] == lines_division[i + 1])
+    {
+      qtd_appears_same_number++;
+      if (qtd_appears_same_number == matrix_order)
+      {
+        return true;
+      }
+    }
+    else
+    {
+      qtd_appears_same_number = 0;
+    }
+  }
+  return false;
+}
+
+boolean can_have_no_zeros_in_main_diagonal(float *matrix) {}
+
+// matrix_order + 1 because the matrix has the results in the last column
+void turn_diagonal_element_into_one(float *matrix, int number_column_position, int number_line_position, int matrix_order)
+{
+  float number = matrix[number_line_position * (matrix_order + 1) + number_column_position];
+  for (int i = 0; i < matrix_order + 1; i++)
+  {
+    matrix[number_column_position * (matrix_order + 1) + i] = matrix[number_column_position * (matrix_order + 1) + i] / number;
+  }
+}
+
+void turn_element_into_zero(float *matrix, int matrix_order, int line_that_just_implemented_one, int current_line, float number_to_multiply)
+{
+  for (int i = 0; i < matrix_order + 1; i++)
+  {
+    float multiplied_number = matrix[line_that_just_implemented_one * (matrix_order + 1) + i] * number_to_multiply;
+    matrix[current_line * (matrix_order + 1) + i] = matrix[current_line * (matrix_order + 1) + i] + multiplied_number;
+  }
+}
+
+// TODO: implement logic to store solutions
+void solve_equation_by_make_column_elements_zero()
+{
+  for (int k = 0; k < sizeof(order_and_matrix); k += 2)
+  {
+    int matrix_order = order_and_matrix[k];
+    float *matrix = matrices[order_and_matrix[k + 1]];
+
+    turn_diagonal_element_into_one(matrix, 0, 0, matrix_order);
+    int line_that_just_implemented_one = 0;
+
+    for (int column = 0; column < matrix_order; column++)
+    {
+      for (int line = 0; line < matrix_order; line++)
+      {
+        if (line != column)
+        {
+          float current_number = matrix[line * (matrix_order + 1) + column];
+          if (current_number != 0.0)
+          {
+            turn_element_into_zero(matrix, matrix_order, line_that_just_implemented_one, line, (current_number * -1.0));
+            if (matrix[line * (matrix_order + 1) + line] != 1.0)
+            {
+              // if we make the element of row N zero, the diagonal NxN becomes one
+              turn_diagonal_element_into_one(matrix, line, line, matrix_order);
+              line_that_just_implemented_one = line;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
 int main(int argument_count, char *argument_values[])
 {
   if (argument_count > 2)
@@ -318,100 +417,6 @@ int main(int argument_count, char *argument_values[])
   return 0;
 }
 
-boolean equal_results_between_pairs_of_lines(float *matrix, int matrix_order)
-{
-  float *lines_division = (float *)malloc(sizeof(float) * (matrix_order * matrix_order));
-  int lines_division_position = 0;
-
-  // i - first line of couple
-  // j - second line of couple
-  // k - interator to divide each element
-  for (int i = 0; i < matrix_order; i++)
-  {
-    for (int j = i + 1; j < matrix_order; j++)
-    {
-      for (int k = 0; k < matrix_order; k++)
-      {
-        float result = matrix[i * (matrix_order + 1) + k] / matrix[j * (matrix_order + 1) + k];
-        lines_division[lines_division_position] = result;
-        lines_division_position++;
-      }
-    }
-  }
-
-  int qtd_appears_same_number = 0;
-  for (int i = 0; i < (matrix_order * matrix_order) - 1; i++)
-  {
-    if (lines_division[i] == lines_division[i + 1])
-    {
-      qtd_appears_same_number++;
-      if (qtd_appears_same_number == matrix_order)
-      {
-        return true;
-      }
-    }
-    else
-    {
-      qtd_appears_same_number = 0;
-    }
-  }
-  return false;
-}
-
-boolean can_have_no_zeros_in_main_diagonal(float *matrix) {}
-
-// matrix_order + 1 because the matrix has the results in the last column
-void turn_diagonal_element_into_one(float *matrix, int number_column_position, int number_line_position, int matrix_order)
-{
-  float number = matrix[number_line_position * (matrix_order + 1) + number_column_position];
-  for (int i = 0; i < matrix_order + 1; i++)
-  {
-    matrix[number_column_position * (matrix_order + 1) + i] = matrix[number_column_position * (matrix_order + 1) + i] / number;
-  }
-}
-
-void turn_element_into_zero(float *matrix, int matrix_order, int line_that_just_implemented_one, int current_line, float number_to_multiply)
-{
-  for (int i = 0; i < matrix_order + 1; i++)
-  {
-    float multiplied_number = matrix[line_that_just_implemented_one * (matrix_order + 1) + i] * number_to_multiply;
-    matrix[current_line * (matrix_order + 1) + i] = matrix[current_line * (matrix_order + 1) + i] + multiplied_number;
-  }
-}
-
-// TODO: implement logic to store solutions
-void solve_equation_by_make_column_elements_zero()
-{
-  for (int k = 0; k < sizeof(order_and_matrix); k += 2)
-  {
-    int matrix_order = order_and_matrix[k];
-    float *matrix = matrices[order_and_matrix[k + 1]];
-
-    turn_diagonal_element_into_one(matrix, 0, 0, matrix_order);
-    int line_that_just_implemented_one = 0;
-
-    for (int column = 0; column < matrix_order; column++)
-    {
-      for (int line = 0; line < matrix_order; line++)
-      {
-        if (line != column)
-        {
-          float current_number = matrix[line * (matrix_order + 1) + column];
-          if (current_number != 0.0)
-          {
-            turn_element_into_zero(matrix, matrix_order, line_that_just_implemented_one, line, (current_number * -1.0));
-            if (matrix[line * (matrix_order + 1) + line] != 1.0)
-            {
-              // if we make the element of row N zero, the diagonal NxN becomes one
-              turn_diagonal_element_into_one(matrix, line, line, matrix_order);
-              line_that_just_implemented_one = line;
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 // https://stackoverflow.com/questions/10874374/passing-a-unknown-size-matrix-reference-to-a-c-function
 // https://www.tutorialspoint.com/cprogramming/c_array_of_pointers.htm
