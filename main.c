d1494ecade2637a536a2d2a58f1235c6262f70ba
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2022-10-18 21:18:43 -0300

    chore: refactor, style and code method

--- main.c
+++ main.c
@@ -1,116 +1,115 @@
 #define _GNU_SOURCE
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #define MAXIMUM 512
 
-typedef enum
-{
-  false,
-  true
-} boolean;
+typedef enum { false, true } boolean;
 
 float **matrices = malloc(MAXIMUM * sizeof(float *));
 int matrices_current_position = 0;
 
 int order_and_matrix[MAXIMUM * 2] = {};
 
-void menu()
-{
+void menu() {
   system("clear");
-  printf(
-      "|-------------------------------------------------|\n"
-      "|       _______                                   |\n"
-      "|       |     __|.---.-.--.--.-----.-----.        |\n"
-      "|       |    |  ||  _  |  |  |__ --|__ --|        |\n"
-      "|       |_______||___._|_____|_____|_____|        |\n"
-      "|                                                 |\n"
-      "|-------------------------------------------------|\n\n");
+  printf("|-------------------------------------------------|\n"
+         "|       _______                                   |\n"
+         "|       |     __|.---.-.--.--.-----.-----.        |\n"
+         "|       |    |  ||  _  |  |  |__ --|__ --|        |\n"
+         "|       |_______||___._|_____|_____|_____|        |\n"
+         "|                                                 |\n"
+         "|-------------------------------------------------|\n\n");
 }
 
-boolean equal_results_between_pairs_of_lines(float *matrix, int matrix_order)
-{
-  float *lines_division = (float *)malloc(sizeof(float) * (matrix_order * matrix_order));
+boolean equal_results_between_pairs_of_lines(float *matrix, int matrix_order) {
+  float *lines_division =
+      (float *)malloc(sizeof(float) * (matrix_order * matrix_order));
   int lines_division_position = 0;
 
   // i - first line of couple
   // j - second line of couple
   // k - interator to divide each element
-  for (int i = 0; i < matrix_order; i++)
-  {
-    for (int j = i + 1; j < matrix_order; j++)
-    {
-      for (int k = 0; k < matrix_order; k++)
-      {
-        float result = matrix[i * (matrix_order + 1) + k] / matrix[j * (matrix_order + 1) + k];
+  for (int i = 0; i < matrix_order; i++) {
+    for (int j = i + 1; j < matrix_order; j++) {
+      for (int k = 0; k < matrix_order; k++) {
+        float result = matrix[i * (matrix_order + 1) + k] /
+                       matrix[j * (matrix_order + 1) + k];
         lines_division[lines_division_position] = result;
         lines_division_position++;
       }
     }
   }
 
-  int qtd_appears_same_number = 0;
-  for (int i = 0; i < (matrix_order * matrix_order) - 1; i++)
-  {
-    if (lines_division[i] == lines_division[i + 1])
-    {
-      qtd_appears_same_number++;
-      if (qtd_appears_same_number == matrix_order)
-      {
+  int appears_same_number_count = 0;
+  for (int i = 0; i < (matrix_order * matrix_order) - 1; i++) {
+    if (lines_division[i] == lines_division[i + 1]) {
+      appears_same_number_count++;
+      if (appears_same_number_count == matrix_order) {
         return true;
       }
-    }
-    else
-    {
-      qtd_appears_same_number = 0;
+    } else {
+      appears_same_number_count = 0;
     }
   }
+
   return false;
 }
 
-boolean can_have_no_zeros_in_main_diagonal(float *matrix) {}
+boolean main_diagonal_has_zeros(float *matrix) {
+  for (int i = 0; i < order_and_matrix[matrices_current_position]; i++) {
+    if (matrix[i * (order_and_matrix[matrices_current_position] + 1)] == 0) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 // matrix_order + 1 because the matrix has the results in the last column
-void turn_diagonal_element_into_one(float *matrix, int number_column_position, int number_line_position, int matrix_order)
-{
-  float number = matrix[number_line_position * (matrix_order + 1) + number_column_position];
-  for (int i = 0; i < matrix_order + 1; i++)
-  {
-    matrix[number_column_position * (matrix_order + 1) + i] = matrix[number_column_position * (matrix_order + 1) + i] / number;
+void turn_diagonal_element_into_one(float *matrix, int number_column_position,
+                                    int number_line_position,
+                                    int matrix_order) {
+  float number = matrix[number_line_position * (matrix_order + 1) +
+                        number_column_position];
+  for (int i = 0; i < matrix_order + 1; i++) {
+    matrix[number_column_position * (matrix_order + 1) + i] =
+        matrix[number_column_position * (matrix_order + 1) + i] / number;
   }
 }
 
-void turn_element_into_zero(float *matrix, int matrix_order, int line_that_just_implemented_one, int current_line, float number_to_multiply)
-{
-  for (int i = 0; i < matrix_order + 1; i++)
-  {
-    float multiplied_number = matrix[line_that_just_implemented_one * (matrix_order + 1) + i] * number_to_multiply;
-    matrix[current_line * (matrix_order + 1) + i] = matrix[current_line * (matrix_order + 1) + i] + multiplied_number;
+void turn_element_into_zero(float *matrix, int matrix_order,
+                            int line_that_just_implemented_one,
+                            int current_line, float number_to_multiply) {
+  for (int i = 0; i < matrix_order + 1; i++) {
+    float multiplied_number =
+        matrix[line_that_just_implemented_one * (matrix_order + 1) + i] *
+        number_to_multiply;
+    matrix[current_line * (matrix_order + 1) + i] =
+        matrix[current_line * (matrix_order + 1) + i] + multiplied_number;
   }
 }
 
-float * solve_equation_by_make_column_elements_zero(float *matrix, int matrix_order)
-{
+float *solve_equation_by_making_column_elements_zero(float *matrix,
+                                                     int matrix_order) {
   float *solutions = (float *)malloc(sizeof(float) * matrix_order);
 
   turn_diagonal_element_into_one(matrix, 0, 0, matrix_order);
   int line_that_just_implemented_one = 0;
 
-  for (int column = 0; column < matrix_order; column++)
-  {
-    for (int line = 0; line < matrix_order; line++)
-    {
-      if (line != column)
-      {
+  for (int column = 0; column < matrix_order; column++) {
+    for (int line = 0; line < matrix_order; line++) {
+      if (line != column) {
         float current_number = matrix[line * (matrix_order + 1) + column];
-        if (current_number != 0.0)
-        {
-          turn_element_into_zero(matrix, matrix_order, line_that_just_implemented_one, line, (current_number * -1.0));
-          if (matrix[line * (matrix_order + 1) + line] != 1.0)
-          {
-            // if we make the element of row N zero, the diagonal NxN becomes one
+        if (current_number != 0.0) {
+          turn_element_into_zero(matrix, matrix_order,
+                                 line_that_just_implemented_one, line,
+                                 (current_number * -1.0));
+          if (matrix[line * (matrix_order + 1) + line] != 1.0) {
+            // if we make the element of row N zero, the diagonal NxN becomes
+            // one
             turn_diagonal_element_into_one(matrix, line, line, matrix_order);
             line_that_just_implemented_one = line;
           }
@@ -119,15 +118,14 @@ float * solve_equation_by_make_column_elements_zero(float *matrix, int matrix_or
     }
   }
 
-  for (int i = 0; i < matrix_order; i++)
-  {
+  for (int i = 0; i < matrix_order; i++) {
     solutions[i] = matrix[i * (matrix_order + 1) + matrix_order];
   }
 
   return solutions;
 }
 
-void free_pointers_array(float** pointers_array, int size) {
+void free_pointers_array(float **pointers_array, int size) {
   for (int i = 0; i < size; i++) {
     free(pointers_array[i]);
   }
@@ -135,87 +133,73 @@ void free_pointers_array(float** pointers_array, int size) {
   free(pointers_array);
 }
 
-int main(int argument_count, char *argument_values[])
-{
-  if (argument_count > 2)
-  {
-    printf("Error: Too many arguments given");
-  }
-  else
-  {
-    if (argument_count == 2)
-    {
-      if (strlen(argument_values[1]) > 4)
-      {
-        if (strcmp(argument_values[1] + strlen(argument_values[1]) - 4, ".txt") != 0)
-        {
-          printf("Error: File is not a .txt file\n");
+int main(int argument_count, char *argument_values[]) {
+  if (argument_count > 2) {
+    printf("Error: Too many arguments");
+  } else {
+    if (argument_count == 2) {
+      if (strlen(argument_values[1]) > 4) {
+        if (strcmp(argument_values[1] + strlen(argument_values[1]) - 4,
+                   ".txt") != 0) {
+          printf("Error: Not a .txt file\n");
           return 1;
         }
-      }
-      else
-      {
-        printf("Error: Invalid file given\n");
+      } else {
+        printf("Error: Invalid file\n");
         return 1;
       }
 
       FILE *file = fopen(argument_values[1], "r");
 
-      if (file == NULL)
-      {
+      if (file == NULL) {
         printf("Error: File could not be opened\n");
         return 1;
-      }
-      else
-      {
+      } else {
         menu();
 
         char line[256];
 
         float *matrix_values;
-        int matrix_values_atual_position = 0;
+        int matrix_values_current_position = 0;
         int matrix_size;
 
-        boolean inserting_equations = false; 
+        boolean inserting_equations = false;
 
-        while (fgets(line, sizeof(line), file) != NULL)
-        {
+        while (fgets(line, sizeof(line), file) != NULL) {
           printf("> Retrieved line of length %zu:\n", strlen(line));
           printf("> %s", line);
 
-          int qtd_line_elements = 0;
+          int line_elements_count = 0;
 
           char *element = strtok(line, " ");
 
-          while (element != NULL)
-          {
+          while (element != NULL) {
             if (isspace(*element) == 0)
-              qtd_line_elements++;
+              line_elements_count++;
             element = strtok(NULL, " ");
           }
 
-          printf("elements %d\n", qtd_line_elements);
+          printf("elements %d\n", line_elements_count);
 
-          if (qtd_line_elements < 1)
-          {
+          if (line_elements_count < 1) {
             // debug
             printf("size : %d\n", matrix_size);
-            printf("values : %d\n", matrix_values_atual_position);
+            printf("values : %d\n", matrix_values_current_position);
             printf("first: %f\n", matrix_values[0]);
-            for (int i = 0; i < (matrix_size); i++)
-            {
+            for (int i = 0; i < (matrix_size); i++) {
               printf("%f ", matrix_values[i]);
             }
 
             /*
-            if (can_have_no_zeros_in_main_diagonal(matrix_values) == true)
-              if (equal_results_between_pairs_of_lines(matrix_values, matrix_size) == false) {
-                matrices[matrices_current_position] = matrix_values;
-                matrices_current_position++;
+            if (main_diagonal_has_zeros(matrix_values))
+              if (equal_results_between_pairs_of_lines(matrix_values,
+            matrix_size) == false) { matrices[matrices_current_position] =
+            matrix_values; matrices_current_position++;
 
-                // vector of pairs -> order and matrix position in matrices array
-                order_and_matrix[matrices_current_position] = matrix_size;
-                order_and_matrix[matrices_current_position + 1] = matrices_current_position;
+                // vector of pairs -> order and matrix position in matrices
+            array order_and_matrix[matrices_current_position] = matrix_size;
+                order_and_matrix[matrices_current_position + 1] =
+            matrices_current_position;
               }
             */
 
@@ -223,55 +207,47 @@ int main(int argument_count, char *argument_values[])
             inserting_equations = 0;
 
             printf("\nReset operation\n");
-          }
-          else if (matrix_size > 0)
-          {
-            if (qtd_line_elements != matrix_size + 1)
-            {
-              printf("Error: Invalid matrix size.");
+          } else if (matrix_size > 0) {
+            if (line_elements_count != matrix_size + 1) {
+              printf("Error: Invalid matrix size");
               return 1;
             }
           }
 
-          if (inserting_equations == true)
-          {
+          if (inserting_equations == true) {
             char *equation_element = strtok(line, " ");
 
-            while (equation_element != NULL)
-            {
-              if (isspace(*equation_element) == 0)
-              {
-                if (isdigit(*equation_element) != 0)
-                {
+            while (equation_element != NULL) {
+              if (isspace(*equation_element) == 0) {
+                if (isdigit(*equation_element) != 0) {
                   float value = atof(equation_element);
-                  matrix_values[matrix_values_atual_position] = value;
-                  matrix_values_atual_position++;
+                  matrix_values[matrix_values_current_position] = value;
+                  matrix_values_current_position++;
                   printf("Value: %f\n", value);
                 }
               }
               equation_element = strtok(NULL, " ");
             }
-          }
-          else if (qtd_line_elements == 1 && inserting_equations == false)
-          {
+          } else if (line_elements_count == 1 && inserting_equations == false) {
             char *first_element = strtok(line, " ");
             sscanf(first_element, "%d", &matrix_size);
-            matrix_values = (float *)malloc(sizeof(float) * (matrix_size * matrix_size + 1));
+            matrix_values = (float *)malloc(sizeof(float) *
+                                            (matrix_size * matrix_size + 1));
             inserting_equations = true;
 
-            printf("Qtd matrix rows: %d\n", matrix_size);
-            printf("inserting equations %d\n", inserting_equations);
+            printf("Matrix size: %d\n", matrix_size);
+            printf("Inserting equations? %d\n", inserting_equations);
           }
         }
 
-        
         /*for (int k = 0; k < sizeof(order_and_matrix); k += 2)
         {
           int matrix_order = order_and_matrix[k];
           float *matrix = matrices[order_and_matrix[k + 1]];
 
           print_matrix(matrix, matrix_order);
-          float *solutions = solve_equation_by_make_column_elements_zero(matrix, matrix_order);
+          float *solutions =
+        solve_equation_by_making_column_elements_zero(matrix, matrix_order);
 
           printf("Solutions: ");
           for (int i = 0; i < matrix_order; i++)
@@ -284,9 +260,7 @@ int main(int argument_count, char *argument_values[])
 
         free_pointers_array(matrices, MAXIMUM);
       }
-    }
-    else
-    {
+    } else {
       printf("Error: No file given\n");
       return 1;
     }
