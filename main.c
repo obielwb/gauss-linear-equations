37a62eef5b1ded9d9d411b27f63ecb2a59aef6f2
João Pedro Barbosa <joaopedrofbarbosa@protonmail.com>
2022-11-03 17:53:51 -0300

    chore: improve permutation

--- main.c
+++ main.c
@@ -81,9 +81,10 @@ void swap(int *a, int *b)
 
 boolean has_zeros_in_diagonal(float *matrix, int matrix_order)
 {
+ // verify if there is a zero in the diagonal
   for (int i = 0; i < matrix_order; i++)
   {
-    if (matrix[i * matrix_order + i] == 0.0)
+    if (matrix[i * (matrix_order + 2)] == 0)
     {
       return true;
     }
@@ -96,15 +97,24 @@ float *arrange_matrix_from_permutation(float *matrix, int *permutation,
                                      int matrix_order)
 {
   float *arranged_matrix =
-      (float *)malloc(matrix_order * matrix_order * sizeof(float));
+      (float *)malloc(matrix_order * (matrix_order + 1) * sizeof(float));
 
   // rows arrangement
   for (int i = 0; i < matrix_order; i++)
   {
-    for (int j = 0; j < matrix_order; j++)
+    for (int j = 0; j < matrix_order + 1; j++)
     {
-      arranged_matrix[i * matrix_order + j] =
-          matrix[permutation[i] * matrix_order + j];
+      arranged_matrix[i * (matrix_order + 1) + j] =
+          matrix[i * (matrix_order + 1) + j];
+    }
+  }
+
+  for (int i = 0; i < matrix_order; i++)
+  {
+    for (int j = 0; j < matrix_order + 1; j++)
+    {
+      arranged_matrix[i * (matrix_order + 1) + j] =
+          matrix[permutation[i] * (matrix_order + 1) + j];
     }
   }
 
@@ -116,10 +126,19 @@ float *arrange_matrix_from_permutation(float *matrix, int *permutation,
   // columns arrangement
   for (int i = 0; i < matrix_order; i++)
   {
+    for (int j = 0; j < matrix_order + 1; j++)
+    {
+      arranged_matrix[i * (matrix_order + 1) + j] =
+          matrix[i * (matrix_order + 1) + j];
+    }
+  }
+
+  for (int i = 0; i < matrix_order; i++)
+  {
     for (int j = 0; j < matrix_order; j++)
     {
-      arranged_matrix[i * matrix_order + j] =
-          matrix[i * matrix_order + permutation[j]];
+      arranged_matrix[j * (matrix_order + 1) + i] =
+          matrix[j * (matrix_order + 1) + permutation[i]];
     }
   }
 
@@ -132,7 +151,7 @@ float *arrange_matrix_from_permutation(float *matrix, int *permutation,
 }
 
 void permutate(int indexes[], int start, int end, int matrix_order,
-               float matrix[matrix_order][matrix_order])
+               float *matrix)
 {
   if (start == end)
   {
@@ -143,11 +162,17 @@ void permutate(int indexes[], int start, int end, int matrix_order,
     {
       for (int i = 0; i < matrix_order; i++)
       {
-        for (int j = 0; j < matrix_order; j++)
+        for (int j = 0; j < matrix_order + 1; j++)
         {
-          matrix[i][j] = arranged_matrix[i * matrix_order + j];
+          // matrix[i * (matrix_order + 1) + j] =
+              // arranged_matrix[i * (matrix_order + 1) + j];
+
+          printf("%.0f ", arranged_matrix[i * (matrix_order + 1) + j]);
         }
+
+        printf("\n");
       }
+
       return;
     }
   }
@@ -174,45 +199,51 @@ boolean remove_main_diagonal_zeros(float *matrix, int matrix_order)
   }
 
   float *matrix_without_zeros =
-      (float *)malloc(matrix_order * matrix_order * sizeof(int));
+      (float *)malloc(matrix_order * (matrix_order + 1) * sizeof(int));
 
   for (int i = 0; i < matrix_order; i++)
   {
-    for (int j = 0; j < matrix_order; j++)
+    for (int j = 0; j < matrix_order + 1; j++)
     {
-      matrix_without_zeros[i * matrix_order + j] = matrix[i * matrix_order + j];
+      matrix_without_zeros[i * (matrix_order + 1) + j] =
+          matrix[i * (matrix_order + 1) + j];
     }
   }
 
   permutate(indexes, 0, matrix_order - 1, matrix_order, matrix_without_zeros);
 
-  int equal_elements = 0;
+  // int equal_elements = 0;
+  // for (int i = 0; i < matrix_order; i++)
+  // {
+  //   for (int j = 0; j < matrix_order + 1; j++)
+  //   {
+  //     if (matrix_without_zeros[i * (matrix_order + 1) + j] ==
+  //         matrix[i * (matrix_order + 1) + j])
+  //     {
+  //       equal_elements++;
+  //     }
+  //   }
+  // }
+
+  // if (equal_elements == matrix_order * matrix_order)
+  // {
+  //   return false;
+  // }
+
   for (int i = 0; i < matrix_order; i++)
   {
-    for (int j = 0; j < matrix_order; j++)
+    for (int j = 0; j < matrix_order + 1; j++)
     {
-      if (matrix_without_zeros[i * matrix_order + j] ==
-          matrix[i * matrix_order + j])
-      {
-        equal_elements++;
-      }
+      // matrix[i * (matrix_order + 1) + j] = matrix_without_zeros[i * (matrix_order + 1) + j];
+
+      printf("%.0f ", matrix_without_zeros[i * (matrix_order + 1) + j]);
     }
+
+    printf("\n");
   }
 
-  if (equal_elements == matrix_order * matrix_order)
-  {
-    return false;
-  }
-
-  for (int i = 0; i < matrix_order; i++)
-  {
-    for (int j = 0; j < matrix_order; j++)
-    {
-      matrix[i * matrix_order + j] = matrix_without_zeros[i * matrix_order + j];
-    }
-  }
-
-  return true;
+  // return true;
+  return false;
 }
 
 // matrix_order + 1 because the matrix has the results in the last column
@@ -293,19 +324,6 @@ void free_pointers_array(float **pointers_array, int size)
   free(pointers_array);
 }
 
-// REMOVE
-void print_matrix(float *matrix, int matrix_order)
-{
-  for (int i = 0; i < matrix_order; i++)
-  {
-    for (int j = 0; j < matrix_order + 1; j++)
-    {
-      printf("%.2f ", matrix[i * (matrix_order + 1) + j]);
-    }
-    printf("\n");
-  }
-}
-
 int main(int argument_count, char *argument_values[])
 {
   if (argument_count > 2)
@@ -370,13 +388,11 @@ int main(int argument_count, char *argument_values[])
             if (isspace(*element) == 0)
             {
               elements[line_elements_count++] = atof(element);
-              printf("%f ", atof(element));
+              // printf("%f ", atof(element));
             }
 
             element = strtok(NULL, " ");
           }
-
-          // printf("elements: %d\n", line_elements_count);
 
           if (line_elements_count < 1)
           {
@@ -389,19 +405,33 @@ int main(int argument_count, char *argument_values[])
             //   printf("%f ", matrix_values[i]);
             // }
 
+            printf("\nremoving zeros\n");
+
             if (remove_main_diagonal_zeros(matrix_values, matrix_size))
             {
+              printf("\nremoved zeros\n");
+
+              for (int i = 0; i < matrix_size; i++)
+              {
+                for (int j = 0; j < matrix_size + 1; j++)
+                {
+                  printf("%f ", matrix_values[i * (matrix_size + 1) + j]);
+                }
+
+                printf("\n");
+              }
+
               if (equal_results_between_pairs_of_lines(matrix_values,
                                                        matrix_size) == false)
               {
-                for (int i = 0; i < matrix_size; i++)
-                {
-                  for (int j = 0; j < matrix_size + 1; j++)
-                  {
-                    printf("%.0f ", matrix_values[i * (matrix_size + 1) + j]);
-                  }
-                  printf("\n");
-                }
+                // for (int i = 0; i < matrix_size; i++)
+                // {
+                //   for (int j = 0; j < matrix_size + 1; j++)
+                //   {
+                //     printf("%.0f ", matrix_values[i * (matrix_size + 1) + j]);
+                //   }
+                //   printf("\n");
+                // }
 
                 matrices[matrices_current_position] = matrix_values;
 
@@ -444,9 +474,6 @@ int main(int argument_count, char *argument_values[])
             matrix_values = (float *)malloc(sizeof(float) *
                                             (matrix_size * matrix_size + 1));
             inserting_equations = true;
-
-            // printf("Matrix size: %d\n", matrix_size);
-            // printf("Inserting equations? %d\n", inserting_equations);
           }
         }
 
@@ -454,43 +481,43 @@ int main(int argument_count, char *argument_values[])
 
         for (int k = 0; k < sizeof(order_and_matrix); k += 2)
         {
-          int matrix_order = order_and_matrix[k];
+          // int matrix_order = order_and_matrix[k];
 
-          if (matrix_order != 0)
-          {
-            float *matrix = matrices[order_and_matrix[k + 1]];
+          // if (matrix_order != 0)
+          // {
+          //   float *matrix = matrices[order_and_matrix[k + 1]];
 
-            printf("System:\n");
-            for (int i = 0; i < matrix_order; i++)
-            {
-              for (int j = 0; j < matrix_order; j++)
-              {
-                printf("%.2f%c ", matrix[i * (matrix_order + 1) + j], 'a' + j);
+          //   printf("System:\n");
+          //   for (int i = 0; i < matrix_order; i++)
+          //   {
+          //     for (int j = 0; j < matrix_order; j++)
+          //     {
+          //       printf("%.2f%c ", matrix[i * (matrix_order + 1) + j], 'a' + j);
 
-                if (j < matrix_order - 1)
-                {
-                  printf("+ ");
-                }
-              }
+          //       if (j < matrix_order - 1)
+          //       {
+          //         printf("+ ");
+          //       }
+          //     }
 
-              printf("= %.2f\n", matrix[i * (matrix_order + 1) + matrix_order]);
-            }
+          //     printf("= %.2f\n", matrix[i * (matrix_order + 1) + matrix_order]);
+          //   }
 
-            float *solutions = solve_equation_by_making_column_elements_zero(matrix, matrix_order);
+          //   float *solutions = solve_equation_by_making_column_elements_zero(matrix, matrix_order);
 
-            printf("\nSolution:\n", k, matrix_order);
+          //   printf("\nSolution:\n", k, matrix_order);
 
-            for (int i = 0; i < matrix_order; i++)
-            {
-              printf("%c = %f\n", 'a' + i, solutions[i]);
-            }
+          //   for (int i = 0; i < matrix_order; i++)
+          //   {
+          //     printf("%c = %f\n", 'a' + i, solutions[i]);
+          //   }
 
-            printf("\n");
-          }
-          else
-          {
-            break;
-          }
+          //   printf("\n");
+          // }
+          // else
+          // {
+          //   break;
+          // }
 
           // float *matrix = matrices[order_and_matrix[k + 1]];
           // printf("matrix %d, order: %d\n", k, matrix_order);
